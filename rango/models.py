9c790e3dde3d45e75b7bfa52676277394a88c78c
4B206S32 <4B206S32>
2015-10-26 14:29:32 +0900

    9.3 Done

--- rango/models.py
+++ rango/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.template.defaultfilters import slugify
-
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -26,4 +26,12 @@ class Page(models.Model):
     def __unicode__(self):      #For Python 2, use __str__ on Python 3
         return self.title
 
+class UserProfile(models.Model):
+    # this
+    user = models.OneToOneField(User)
 
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __unicode__(self):
+        return self.user.username
